2b40697c68561c8921b1d0c8003ffc36773115ad
QmelZ <qmelzthevoid@gmail.com>
2021-03-16 15:52:12 +0300

    might work

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,11 @@
 let die = loadMusic("die");
 
-function iterateChildren(p){
-    print(1);
-    if(
-        p === null ||
-        p === undefined ||
-        p.children === null ||
-        p.children === undefined
-    ) return;
-    
-    print(2)
-    p.children.each(e => {
-        if(e instanceof Button){
-            print(3);
-            try{
-                print(4);
-                let l = extend(ChangeListener, {
-                    changed(){
-                        die.setVolume(1);
-                        die.play();
-                        Timer.schedule(() => Core.app.exit(), 1);
-                    }
-                });
-                e.addListener(l);
-            }catch(c){}
-        }else{
-            print(5);
-            try{
-                iterateChildren(e);
-            }catch(c){}
+Events.run(Trigger.update, () => {
+    if(Core.input.justTouched()){
+        if(Mathf.randomBoolean(0.02)){
+            die.setVolume(100);
+            die.play();
+            Timer.schedule(() => Core.app.exit(), 1);
         }
-    });
-}
-
-Events.on(ClientLoadEvent, () => {
-    iterateChildren(Vars.ui.menuGroup);
+    }
 });
